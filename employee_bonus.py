30252ec61dc81d6938b99402793fca7da0b2c39a
Matt <msmith@test.com>
2022-09-08 22:52:34 -0500

    fixed format on employee bonus

--- employee_bonus.py
+++ employee_bonus.py
@@ -11,14 +11,14 @@ for record in csv_file:
 
     id = record[0]
     full_name = record[1] + " " + record[2]
-    salary = float(record[3])
+    salary = int(record[3])
     bonus = (float(record[4]) * 100)
 
-    total_pay = format( salary + (salary * bonus), "," ) 
+    total_pay = format( salary + (salary * int(bonus)), "," ) 
 
     print("ID:", id)
     print("Full Name:", full_name)
-    print("Salary:", format(salary, ","))
+    print("Salary:" + " $" +format(salary, ","))
     print("Bonus: " + str(int(bonus)) + "%")
     print("Total Pay: " + "$" + str(total_pay))
     
